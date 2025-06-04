b5c2fb05591307fdc893c0febc7985f6c239da3b
Fabian <Fabian.Grasboeck07@htl-salzburg.ac.at>
2025-06-04 09:44:50 +0200

    lical to git

---
+++ Assets/Scripts/RegEx.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using TMPro;
+
+using System.Text.RegularExpressions;
+
+public class RegEX : MonoBehaviour
+{
+    public TMP_InputField emailInput;
+    public TMP_Text resultText;
+
+
+    void Start()
+    {
+        resultText.text = "";
+    }
+
+    public void ValidateEmail()
+    {
+        string email = emailInput.text;
+        string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+        if (Regex.IsMatch(email, pattern))
+        {
+            resultText.text = "✅ Gültige E-Mail-Adresse!";
+            resultText.color = Color.green;
+        }
+        else
+        {
+            resultText.text = "❌ Ungültige E-Mail-Adresse!";
+            resultText.color = Color.red;
+        }
+    }
+}
